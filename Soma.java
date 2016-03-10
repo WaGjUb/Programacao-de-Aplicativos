b8c0a63902c275901f1ad04139a8885b658bc3c5
wagjub <danielcostavalerio@hotmail.com>
2016-03-10 15:30:49 -0300

    soma

---
+++ Soma.java
@@ -0,0 +1,22 @@
+//package funcaoSoma;
+
+public class Soma{
+
+  public static void main(String[] args){
+
+  //    System.out.println("digite 2 valores: ");
+    System.out.println("resultado da soma: " + Soma(1,2));
+  }
+
+  static boolean Soma(int a, int b)
+  {
+    Integer i1 = 100;
+//cria um inteiro empacotado
+Integer i2 = new Integer(100);
+//cria um inteiro empacotado
+Integer i3 = new Integer("100");
+//cria um inteiro empacotado
+Integer i4 = Integer.valueOf("100", 2);
+//cria um inteiro empacotado
+  return (i1==i2);
+}}
